58cd8a0509adb51c471fdc83c536016d3aae789f
MoyTW <moytravis@gmail.com>
2020-08-13 22:37:36 -0700

    Hack example
    See: https://konvajs.org/docs/react/Intro.html

--- src/components/RoomListView.tsx
+++ src/components/RoomListView.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Room } from '../room'
 import { moveToRoom } from '../networking'
 import MenuButtonView from './MenuButtonView'
+import { Stage, Layer, Star, Text } from 'react-konva'
 
 import '../../style/nav.css'
 
@@ -10,15 +11,53 @@ interface Props {
   username: string;
 }
 
+function generateShapes () {
+  return [...Array(4)].map((_, i) => ({
+    id: i.toString(),
+    x: Math.random() * 200,
+    y: Math.random() * 200,
+    rotation: Math.random() * 180,
+    isDragging: false
+  }))
+}
+
+const INITIAL_STATE = generateShapes()
+
 export default function RoomListView (props: Props) {
+  const stars = generateShapes()
+  console.log('STARS')
+  console.log(stars)
+
   return (
     <nav id="side-menu" role="navigation" aria-label="List of rooms you can navigate to">
       <MenuButtonView username={props.username} />
-      <ul>
-        {props.rooms.map((r) => (
-          <RoomListItem room={r} key={`room-sidebar-${r.id}`} />
-        ))}
-      </ul>
+      <Stage width={200} height={200}>
+        <Layer>
+          <Text text="Try to drag a star" />
+          {stars.map((star) => (
+            <Star
+              key={star.id}
+              id={star.id}
+              x={star.x}
+              y={star.y}
+              numPoints={5}
+              innerRadius={20}
+              outerRadius={40}
+              fill="#89b717"
+              opacity={0.8}
+              draggable
+              rotation={star.rotation}
+              shadowColor="black"
+              shadowBlur={10}
+              shadowOpacity={0.6}
+              shadowOffsetX={star.isDragging ? 10 : 5}
+              shadowOffsetY={star.isDragging ? 10 : 5}
+              scaleX={star.isDragging ? 1.2 : 1}
+              scaleY={star.isDragging ? 1.2 : 1}
+            />
+          ))}
+        </Layer>
+      </Stage>
     </nav>
   )
 }
